b0d2589aa8da856e6aaa84e0c2bb53b05e44bac4
groovestation31785 <angroover@gmail.com>
2017-06-21 16:24:31 -0700

    Update helpers for spec assistance

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec-rails'
 
 RSpec.configure do |config|
   config.include ApiHelper, type: :api
-  config.include Requests::JsonHelpers, type: :api
+  config.include Requests::JsonHelpers, type: :request
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
