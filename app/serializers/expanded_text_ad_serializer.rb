b881dcb6dc741ef68c80469795c98f6acf362a4c
groovestation31785 <angroover@gmail.com>
2017-06-21 16:18:28 -0700

    Add relationships to all serializers

--- app/serializers/expanded_text_ad_serializer.rb
+++ app/serializers/expanded_text_ad_serializer.rb
@@ -1,3 +1,4 @@
 class ExpandedTextAdSerializer < ActiveModel::Serializer
+  belongs_to :ad_group
   attributes :id, :xsi_type, :ad_group_id, :headline_part1, :headline_part2, :description, :path1, :path2
 end
