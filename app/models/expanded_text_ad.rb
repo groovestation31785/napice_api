39d93a796e1751f08f2a18bfe92ae4d7b0120654
groovestation31785 <angroover@gmail.com>
2017-06-21 17:04:36 -0700

    Allow headline_part2 to be nil

--- app/models/expanded_text_ad.rb
+++ app/models/expanded_text_ad.rb
@@ -1,6 +1,6 @@
 class ExpandedTextAd < ApplicationRecord
   belongs_to :ad_group
 
-  validates_presence_of :xsi_type, :ad_group_id, :headline_part1, :headline_part2, :description, :path1, :path2
+  validates_presence_of :xsi_type, :ad_group_id, :headline_part1, :description, :path1, :path2
   validates_numericality_of :ad_group_id
 end
--- app/serializers/expanded_text_ad_serializer.rb
+++ app/serializers/expanded_text_ad_serializer.rb
@@ -1,4 +1,4 @@
 class ExpandedTextAdSerializer < ActiveModel::Serializer
   belongs_to :ad_group
-  attributes :id, :xsi_type, :ad_group_id, :headline_part1, :headline_part2, :description, :path1, :path2
+  attributes :id, :xsi_type, :ad_group_id, :headline_part1, :description, :path1, :path2
 end
