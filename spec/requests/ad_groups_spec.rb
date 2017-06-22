f65cc3e229a61990b1a5e969f2947860657d3759
groovestation31785 <angroover@gmail.com>
2017-06-21 17:03:12 -0700

    Begin spec tests for other controllers

--- spec/requests/ad_groups_spec.rb
+++ spec/requests/ad_groups_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "AdGroups", type: :request do
+RSpec.describe "AdGroups API", type: :request do
+  let!(:ad_group) { create(:ad_group) }
+
   describe "GET /ad_groups" do
-    it "works! (now write some real specs)" do
-      get ad_groups_path
+    before { get '/api/v1/ad_groups' }
+
+    it "sends a list of ad groups" do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code of 200' do
       expect(response).to have_http_status(200)
     end
   end
